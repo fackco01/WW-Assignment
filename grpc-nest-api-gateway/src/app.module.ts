dc667870434db15248a212c97dc9cadd1531b828
fackco01 <duanhotrong@gmail.com>
2024-08-16 21:07:42 +0700

    Update Proto

--- grpc-nest-api-gateway/src/app.module.ts
+++ grpc-nest-api-gateway/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PostModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
